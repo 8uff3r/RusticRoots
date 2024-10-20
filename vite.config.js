2137e0e188fb16f4d578d56b47f13aad5bd12412
8uff3r <8uff3r@gmail.com>
2024-10-21 00:24:47 +0330

    set vite to https for production

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
+  server: {
+    https: process.env.NODE_ENV === 'development' ? false : true
+  },
   plugins: [
     laravel({
       input: 'resources/js/app.ts',
