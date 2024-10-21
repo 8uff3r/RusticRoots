05778af45bb55af85e4a90c7606f6a2e51cb0efa
8uff3r <8uff3r@gmail.com>
2024-10-22 01:19:44 +0330

    fix axios credentials

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-//@ts-ignore
-window.axios.defaults.headers.common['X-CSRF-TOKEN'] = window.Laravel.csrfToken;
+window.axios.defaults.withCredentials = true;
+window.axios.defaults.withXSRFToken = true;
