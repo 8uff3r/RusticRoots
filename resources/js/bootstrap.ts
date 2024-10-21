e014fe9b83b3b87689062c24cdd192dcf00a3b4a
8uff3r <8uff3r@gmail.com>
2024-10-21 23:23:17 +0330

    fix csrf token mismatch

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -2,3 +2,5 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+//@ts-ignore
+window.axios.defaults.headers.common['X-CSRF-TOKEN'] = window.Laravel.csrfToken;
